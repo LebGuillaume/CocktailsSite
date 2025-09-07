e4173a4be5096c9423597720fed3dd0aeea2c4e8
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2025-09-07 19:27:56 +0200

    ajout composant Hero + Assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import gsap from 'gsap';
 import {navLinks} from "../../constants/index.js";
+import {useGSAP} from "@gsap/react";
 
 const Navbar = () => {
+
+  useGSAP(()=>{
+    const nawTween = gsap.timeline({
+      scrollTrigger: {
+        trigger:'nav',
+        start: 'bottom top',
+      }
+
+    });
+
+    navTween.fromTo('nav',
+      {
+        backgoundColor: 'transparent'},
+      {
+        backgroundColor: '#00000050',
+        backgroundFilter: 'blur(10px)',
+        duration: 1,
+        ease: 'power1.inOut'
+  })
+  })
   return (
     <nav>
       <div>
         <a href="#home" className="flex items-center gap-2">
+          <img src='/images/logo.png' alt='Logo' />
           <p>Velvet pour</p>
         </a>
         <ul>
