85e90bcea8d45cfb352823a6f077de17365a860a
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2025-09-16 16:23:30 +0200

    Ajout section Art avec animation du masque

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import gsap from 'gsap';
-import { ScrollTrigger, SplitText} from "gsap/all";
+import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Coktails from "./components/cocktails.jsx";
+import About from "./components/about.jsx";
+import Art from "./components/Art.jsx";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main className="App">
-      <Navbar />
-
+      <Navbar/>
+      <Hero/>
+      <Coktails/>
+      <About/>
+      <Art/>
+      <div className="h-dvh color-black"></div>
 
     </main>
   )
